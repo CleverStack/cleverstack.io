037a858ee78f7bb48cb35ead16eff8a7a2b3c774
Daniel Durante <me@danieldurante.com>
2014-02-12 23:41:28 -0500

    Changes cursive font into lato, fixes scroll, and some typos.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
    jQuery(document).ready(function() {
     var stickyNavTop = jQuery('.sub-nav-wrapper');
     if (stickyNavTop.length > 0) {
-      stickyNavTop = stickyNav.offset().top;
+      stickyNavTop = stickyNavTop.offset().top;
 
       var stickyNav = function(){
         var scrollTop = jQuery(window).scrollTop();
@@ -37,12 +37,13 @@ jQuery(document).ready(function() {
 
    jQuery(".scroll").click(function(event){
      event.preventDefault();
+     var stickyNavTop = jQuery('.sub-nav-wrapper');
          //calculate destination place
          var dest=0;
          if(jQuery(this.hash).offset().top > jQuery(document).height()-jQuery(window).height()){
           dest=jQuery(document).height()-jQuery(window).height();
         }else{
-          dest=jQuery(this.hash).offset().top-150;
+          dest=jQuery(this.hash).offset().top - (stickyNavTop.length > 0 ? 120 : 0);
         }
          //go to destination
          jQuery('html,body').animate({scrollTop:dest}, 1000,'swing');
