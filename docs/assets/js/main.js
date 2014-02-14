192021dafd3b175e14242c6dd22e9bc3b46f506a
Daniel Durante <me@danieldurante.com>
2014-02-14 17:07:38 -0500

    Adds Google Analytics.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -8,6 +8,53 @@ jQuery(document).ready(function() {
 })();
  //edit, if you want to use this variable outside of this closure, or later use this:
  window.isDesktop = isDesktop;
+
+ jQuery(document).ready(function() {
+if ($('#player').length > 0) {
+var player;  function onYouTubePlayerAPIReady() {  player = new YT.Player('player', {  height: '420',  width: '640',  videoId: 'h2-kSRrHHGo',  events: {  'onReady': onPlayerReady,  'onStateChange': onPlayerStateChange  }  });  }
+function onPlayerReady(event) {
+// event.target.playVideo();
+}
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING)  {
+    ga('send', 'event', 'Introduction Video', 'click', 'Play');
+  }
+
+  if (event.data == YT.PlayerState.ENDED)  {
+    ga('send', 'event', 'Introduction Video', 'click', 'Watch to End');
+  }
+}
+}
+
+  $('#knows-npm').bind('click', function () {
+    ga('send', 'event', 'Frontpage Navigation', 'click', 'Developer');
+  });
+  $('#knows-business').bind('click', function() {
+    ga('send', 'event', 'Frontpage Navigation', 'click', 'Business');
+  });
+  $('#fork-github').bind('click', function() {
+    ga('send', 'event', 'Developer Page', 'click', 'Fork it on GitHub');
+  });
+  $('.get-started').bind('click', function() {
+    ga('send', 'event', 'Developer Page', 'click', 'Get Started - ' + $(this).data('name'));
+  });
+  $('.get-started-biz').bind('click', function() {
+    ga('send', 'event', 'Business Page', 'click', 'Get Started - ' + $(this).data('name'));
+  });
+  $('.biz-nav').bind('click', function() {
+    ga('send', 'event', 'Business Page', 'click', 'Business Navigation - ' + $(this).data('name'));
+  });
+  $('.biz-nav-top').bind('click', function() {
+    ga('send', 'event', 'Business Page', 'click', 'Business Top Navigation - ' + $(this).data('name'));
+  });
+  $('#contact-ct-biz').bind('click', function() {
+    ga('send', 'event', 'Business Page', 'click', 'Contact Clevertech');
+  });
+  $('#why-video').bind('click', function() {
+    ga('send', 'event', 'Developer Page', 'click', 'Why Cleverstack Video Link');
+  });
+ });
+
  if( isDesktop ){
    /* desktop things */
 
